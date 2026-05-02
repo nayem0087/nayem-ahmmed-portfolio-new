02e376a49997bbe2c72e76a713b9af9710887cdd
Nayem Ahmed <nayemk0087@gmail.com>
2026-05-02 10:35:34 +0600

    added my hero section in my portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-// import Hero from '@/components/Hero';
+import Hero from '@/components/Hero';
 import About from '@/components/About';
 // import TechStack from '@/components/TechStack';
 // import Skills from '@/components/Skills';
 // import Projects from '@/components/Projects';
 // import Qualification from '@/components/Qualification';
-// import Footer from '@/components/Footer';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
     <main>
-      {/* <Hero /> */}
+      <Hero />
       <About />
       {/* <TechStack />
       <Skills />
       <Projects />
-      <Qualification />
-      <Footer /> */}
+      <Qualification /> */}
+      <Footer />
     </main>
   );
 }
